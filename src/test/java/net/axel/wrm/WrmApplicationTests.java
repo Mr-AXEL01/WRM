3a800a9442c6cbefaaa89ff653985e7e6de52752
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-11 15:20:47 +0100

    fc

---
+++ src/test/java/net/axel/wrm/WrmApplicationTests.java
@@ -0,0 +1,13 @@
+package net.axel.wrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
