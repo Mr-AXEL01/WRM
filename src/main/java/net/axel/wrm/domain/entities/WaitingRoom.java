80db003410e5a6124b82f024979c045f6ec4bc95
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-12 13:51:19 +0100

    Chore: remove attribute
    remove the partTime attribute.

--- src/main/java/net/axel/wrm/domain/entities/WaitingRoom.java
+++ src/main/java/net/axel/wrm/domain/entities/WaitingRoom.java
@@ -4,11 +4,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.axel.wrm.domain.enums.Mode;
-import net.axel.wrm.domain.enums.PartTime;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -39,9 +37,6 @@ public class WaitingRoom implements Serializable {
 
     @Enumerated(EnumType.STRING)
     private Mode mode;
-
-    @Enumerated(EnumType.STRING)
-    private PartTime partTime;
 
     @OneToMany(mappedBy = "waitingRoom")
     private Set<Visit> visits;
