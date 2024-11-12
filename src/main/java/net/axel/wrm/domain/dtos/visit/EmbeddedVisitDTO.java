aad7982b729681f9df0e4590f6b192ae1326d520
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-12 16:25:54 +0100

    Refactor : change the id data type
    update id data type Long -> VisitKey.

--- src/main/java/net/axel/wrm/domain/dtos/visit/EmbeddedVisitDTO.java
+++ src/main/java/net/axel/wrm/domain/dtos/visit/EmbeddedVisitDTO.java
@@ -1,12 +1,13 @@
 package net.axel.wrm.domain.dtos.visit;
 
 import jakarta.validation.constraints.NotNull;
+import net.axel.wrm.domain.embeddeds.VisitKey;
 import net.axel.wrm.domain.enums.Status;
 
 import java.time.LocalDateTime;
 
 public record EmbeddedVisitDTO(
-        @NotNull Long id,
+        @NotNull VisitKey id,
         @NotNull LocalDateTime arrivalTime,
         LocalDateTime startTime,
         LocalDateTime endTime,
--- src/main/java/net/axel/wrm/domain/dtos/visit/VisitResponseDTO.java
+++ src/main/java/net/axel/wrm/domain/dtos/visit/VisitResponseDTO.java
@@ -3,13 +3,14 @@ package net.axel.wrm.domain.dtos.visit;
 import jakarta.validation.constraints.NotNull;
 import net.axel.wrm.domain.dtos.visitor.EmbeddedVisitorDTO;
 import net.axel.wrm.domain.dtos.waitingRoom.EmbeddedWaitingRoomDTO;
+import net.axel.wrm.domain.embeddeds.VisitKey;
 import net.axel.wrm.domain.enums.Status;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 public record VisitResponseDTO(
-        @NotNull Long id,
+        @NotNull VisitKey id,
         @NotNull LocalDateTime arrivalTime,
         LocalDateTime startTime,
         LocalDateTime endTime,
