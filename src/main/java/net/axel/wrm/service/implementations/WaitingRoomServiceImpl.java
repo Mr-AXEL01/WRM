526be593fbe8c72ecaf6078e2c4d64c713b02ee4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-13 22:46:39 +0100

    implement methods from the interface
    implements methods declared in the interface of waitingRoomService.

--- src/main/java/net/axel/wrm/service/implementations/WaitingRoomServiceImpl.java
+++ src/main/java/net/axel/wrm/service/implementations/WaitingRoomServiceImpl.java
@@ -2,10 +2,14 @@ package net.axel.wrm.service.implementations;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomRequestDTO;
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomResponseDTO;
 import net.axel.wrm.mapper.WaitingRoomMapper;
 import net.axel.wrm.repository.WaitingRoomRepository;
 import net.axel.wrm.service.WaitingRoomService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,4 +19,29 @@ public class WaitingRoomServiceImpl implements WaitingRoomService {
 
     private final WaitingRoomRepository repository;
     private final WaitingRoomMapper mapper;
+
+    @Override
+    public List<WaitingRoomResponseDTO> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public WaitingRoomResponseDTO getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public WaitingRoomResponseDTO create(WaitingRoomRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public WaitingRoomResponseDTO update(Long id, WaitingRoomRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
