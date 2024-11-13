fc40a9dba0ce10497f766ca1279b6d4233449e5a
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-13 22:43:24 +0100

    Feat : add service interface
    declare the methods of the waitingRoomService

--- src/main/java/net/axel/wrm/service/WaitingRoomService.java
+++ src/main/java/net/axel/wrm/service/WaitingRoomService.java
@@ -1,4 +1,19 @@
 package net.axel.wrm.service;
 
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomRequestDTO;
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomResponseDTO;
+
+import java.util.List;
+
 public interface WaitingRoomService {
+
+    List<WaitingRoomResponseDTO> getAll();
+
+    WaitingRoomResponseDTO getById(Long id);
+
+    WaitingRoomResponseDTO create(WaitingRoomRequestDTO dto);
+
+    WaitingRoomResponseDTO update(Long id, WaitingRoomRequestDTO dto);
+
+    void delete(Long id);
 }
