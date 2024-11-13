813d8a74bb26fd8160faf162d29d08f592e2e306
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-13 22:11:15 +0100

    Feat : add spring doc api
    set the config for the spring doc api by setting proprties trelated to the app.

---
+++ src/main/java/net/axel/wrm/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package net.axel.wrm.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Development");
+
+        Contact myContact = new Contact();
+        myContact.setName("Mr-AXEL01");
+        myContact.setEmail("abdelhakazrour3@gmail.com");
+
+        Info information = new Info()
+                .title("WRM System API")
+                .version("1.0")
+                .description("This API exposes endpoints to manage Waiting rooms.")
+                .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
+}
