87897f4bce6cc59fcc16cfb499ad7adbc96ee2fe
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-14 22:50:39 +0100

    add new class the enabled configuration proprties
    add the VisitConfigProprties class

--- src/main/java/net/axel/wrm/config/AppConfig.java
+++ src/main/java/net/axel/wrm/config/AppConfig.java
@@ -1,10 +1,11 @@
 package net.axel.wrm.config;
 
+import net.axel.wrm.config.applicationProprtiesConfig.VisitConfigProperties;
 import net.axel.wrm.config.applicationProprtiesConfig.WaitingRoomConfigProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({WaitingRoomConfigProperties.class})
+@EnableConfigurationProperties({WaitingRoomConfigProperties.class, VisitConfigProperties.class})
 public class AppConfig {
 }
