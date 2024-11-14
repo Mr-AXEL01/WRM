6e80eaa4bb2052bdaca0177031903f14969c520f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-14 22:52:21 +0100

    add new attributes to the request dto
    add the priority && ept attributes to the request dto of the visit.

--- src/main/java/net/axel/wrm/domain/dtos/visit/VisitRequestDTO.java
+++ src/main/java/net/axel/wrm/domain/dtos/visit/VisitRequestDTO.java
@@ -3,11 +3,14 @@ package net.axel.wrm.domain.dtos.visit;
 import jakarta.validation.constraints.NotNull;
 import net.axel.wrm.domain.enums.Status;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public record VisitRequestDTO(
         @NotNull LocalDateTime arrivalTime,
         @NotNull Status status,
+        Integer priority,
+        Duration ept,
         @NotNull Long visitorId,
         @NotNull Long waitingRoomId
 ) {
