c987311e82ea608fc1bae7b4a618cf53f9976574
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-15 14:24:33 +0100

    change the id passed in the repository

--- src/main/java/net/axel/wrm/repository/VisitRepository.java
+++ src/main/java/net/axel/wrm/repository/VisitRepository.java
@@ -1,9 +1,10 @@
 package net.axel.wrm.repository;
 
+import net.axel.wrm.domain.embeddeds.VisitKey;
 import net.axel.wrm.domain.entities.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VisitRepository extends JpaRepository<Visit, Long> {
+public interface VisitRepository extends JpaRepository<Visit, VisitKey> {
 }
