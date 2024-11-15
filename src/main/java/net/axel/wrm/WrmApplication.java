e3d77aa856ef3b06943485af27d16e2089d3c244
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-15 09:23:11 +0100

    add a command line runner method
    set a command line runner method that return the creation of visitors on the start of project.

--- src/main/java/net/axel/wrm/WrmApplication.java
+++ src/main/java/net/axel/wrm/WrmApplication.java
@@ -1,13 +1,28 @@
 package net.axel.wrm;
 
+import net.axel.wrm.domain.entities.Visitor;
+import net.axel.wrm.repository.VisitorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WrmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WrmApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(VisitorRepository visitorRepository) {
+        return args -> {
+            visitorRepository.save(Visitor.builder().firstName("Aymane").lastName("El maini").build());
+            visitorRepository.save(Visitor.builder().firstName("Soufiane").lastName("Bouanani").build());
+            visitorRepository.save(Visitor.builder().firstName("Hamza").lastName("Lamin").build());
+            visitorRepository.save(Visitor.builder().firstName("Mohammed").lastName("Daali").build());
+            visitorRepository.save(Visitor.builder().firstName("Zakaria").lastName("El hassad").build());
+        };
     }
 
 }
