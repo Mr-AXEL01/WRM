b7decfe716ff7c5afb9e4ff97b10e7f19764d79d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-15 11:37:07 +0100

    add controller
    set the controller for the waitingRoom and handle the basic endpoints for the CRUD

--- src/main/java/net/axel/wrm/controller/WaitingRoomController.java
+++ src/main/java/net/axel/wrm/controller/WaitingRoomController.java
@@ -1,4 +1,56 @@
 package net.axel.wrm.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomRequestDTO;
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomResponseDTO;
+import net.axel.wrm.service.WaitingRoomService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(WaitingRoomController.CONTROLLER_PATH)
+
+@RequiredArgsConstructor
 public class WaitingRoomController {
+    public final static String CONTROLLER_PATH = "/api/v1/waiting-rooms";
+
+    private final WaitingRoomService service;
+
+    @GetMapping
+    public ResponseEntity<List<WaitingRoomResponseDTO>> findAll() {
+        List<WaitingRoomResponseDTO> waitingRooms = service.getAll();
+        return ResponseEntity.ok(waitingRooms);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WaitingRoomResponseDTO> findById(
+            @PathVariable("id") Long id) {
+        WaitingRoomResponseDTO waitingRoom = service.getById(id);
+        return ResponseEntity.ok(waitingRoom);
+    }
+
+    @PostMapping
+    public ResponseEntity<WaitingRoomResponseDTO> create(
+            @RequestBody @Valid WaitingRoomRequestDTO dto) {
+        WaitingRoomResponseDTO createdWaitingRoom = service.create(dto);
+        return new ResponseEntity<>(createdWaitingRoom, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WaitingRoomResponseDTO> update(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid WaitingRoomRequestDTO dto) {
+        WaitingRoomResponseDTO updatedWaitingRoom = service.update(id, dto);
+        return ResponseEntity.ok(updatedWaitingRoom);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
