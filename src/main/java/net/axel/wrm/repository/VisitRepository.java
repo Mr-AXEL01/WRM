e74608d87497617e8f363630148dce859e2c0637
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-16 03:26:23 +0100

    declare new method to find the visits depeands waitingRoomId

--- src/main/java/net/axel/wrm/repository/VisitRepository.java
+++ src/main/java/net/axel/wrm/repository/VisitRepository.java
@@ -2,9 +2,13 @@ package net.axel.wrm.repository;
 
 import net.axel.wrm.domain.embeddeds.VisitKey;
 import net.axel.wrm.domain.entities.Visit;
+import net.axel.wrm.domain.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, VisitKey> {
+    List<Visit> findVisitsByWaitingRoomIdAndStatus(Long waitingRoomId, Status status);
 }
