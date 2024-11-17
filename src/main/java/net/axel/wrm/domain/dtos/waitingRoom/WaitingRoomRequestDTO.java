b2e716493840bfa8b78461e3a5279fb5434a6327
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-17 04:13:30 +0100

    remove date from request dto and handle it on service

--- src/main/java/net/axel/wrm/domain/dtos/waitingRoom/WaitingRoomRequestDTO.java
+++ src/main/java/net/axel/wrm/domain/dtos/waitingRoom/WaitingRoomRequestDTO.java
@@ -7,9 +7,8 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public record WaitingRoomRequestDTO(
-        LocalDate date,
         String algorithm,
         Integer capacity,
-        @NotNull Mode mode
+        Mode mode
 ) {
 }
