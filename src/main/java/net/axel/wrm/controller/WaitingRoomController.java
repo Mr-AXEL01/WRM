8dbe712ea049f7352e68436a76b0e94331b12359
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-17 04:08:34 +0100

    add new endpoint
    set new endpoint point for statistics

--- src/main/java/net/axel/wrm/controller/WaitingRoomController.java
+++ src/main/java/net/axel/wrm/controller/WaitingRoomController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomRequestDTO;
 import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomResponseDTO;
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomStatisticsDTO;
 import net.axel.wrm.service.WaitingRoomService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -52,5 +53,11 @@ public class WaitingRoomController {
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/statistics")
+    public ResponseEntity<WaitingRoomStatisticsDTO> getStatistics(@PathVariable("id") Long id) {
+        WaitingRoomStatisticsDTO statistics = service.generateStatics(id);
+        return ResponseEntity.ok(statistics);
     }
 }
