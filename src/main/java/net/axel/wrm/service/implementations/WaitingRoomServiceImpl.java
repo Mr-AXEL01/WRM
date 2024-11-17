665ca659ebd377a86849323e54882022880e3a59
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-17 04:54:49 +0100

    set methods for the statistics
    set methods to calcul statistics and call them in a main method generateStatistics()

--- src/main/java/net/axel/wrm/service/implementations/WaitingRoomServiceImpl.java
+++ src/main/java/net/axel/wrm/service/implementations/WaitingRoomServiceImpl.java
@@ -5,14 +5,19 @@ import lombok.RequiredArgsConstructor;
 import net.axel.wrm.config.applicationProprtiesConfig.WaitingRoomConfigProperties;
 import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomRequestDTO;
 import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomResponseDTO;
+import net.axel.wrm.domain.dtos.waitingRoom.WaitingRoomStatisticsDTO;
+import net.axel.wrm.domain.entities.Visit;
 import net.axel.wrm.domain.entities.WaitingRoom;
 import net.axel.wrm.mapper.WaitingRoomMapper;
 import net.axel.wrm.repository.WaitingRoomRepository;
 import net.axel.wrm.service.WaitingRoomService;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -56,10 +61,9 @@ public class WaitingRoomServiceImpl implements WaitingRoomService {
     @Override
     public WaitingRoomResponseDTO update(Long id, WaitingRoomRequestDTO dto) {
         WaitingRoom waitingRoom = mapper.toEntityFromResponseDto(getById(id))
-                .setDate(dto.date())
-                .setAlgorithm(getDefult(dto.algorithm(), waitingRoomConfigProperties.algorithm()))
-                .setCapacity(getDefult(dto.capacity(), waitingRoomConfigProperties.capacity()))
-                .setMode(getDefult(dto.mode(), waitingRoomConfigProperties.mode()));
+                .setAlgorithm(dto.algorithm())
+                .setCapacity(dto.capacity())
+                .setMode(dto.mode());
         WaitingRoom updatedWaitingRoom = repository.save(waitingRoom);
         return mapper.toResponseDto(updatedWaitingRoom);
     }
@@ -69,8 +73,30 @@ public class WaitingRoomServiceImpl implements WaitingRoomService {
         repository.deleteById(id);
     }
 
+    @Override
+    public WaitingRoomStatisticsDTO generateStatics(Long id) {
+        WaitingRoomResponseDTO waitingRoomResponse = getById(id);
+        WaitingRoom waitingRoom = mapper.toEntityFromResponseDto(waitingRoomResponse);
+        Set<Visit> visits = waitingRoom.getVisits();
+
+        Double averageWaitingTime = calculateAverageWaitingTime(visits);
+        Integer visitorRotation = calculateVisitorRotation(visits);
+        return new WaitingRoomStatisticsDTO(waitingRoom.getDate(), waitingRoom.getAlgorithm(), waitingRoom.getMode(), averageWaitingTime, visitorRotation);
+    }
+
     private <T> T getDefult(T v, T dv) {
         return v != null ? v : dv;
+    }
+    
+    private Double calculateAverageWaitingTime(Set<Visit> visits) {
+        return visits.stream()
+                .filter(visit -> visit.getArrivalTime() != null && visit.getStartTime() != null)
+                .mapToLong(visit -> Duration.between(visit.getArrivalTime(), visit.getStartTime()).toMinutes())
+                .average().orElse(0);
+    }
+
+    private Integer calculateVisitorRotation(Set<Visit> visits) {
+        return visits.size();
     }
 
 }
