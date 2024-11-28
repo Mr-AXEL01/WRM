f047357edb3ec55da9c62f70015df5c954461ae9
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-28 14:16:05 +0100

    Style : style code
    remove unused imports , style code for more readability.

--- src/main/java/net/axel/wrm/domain/dtos/visit/VisitRequestDTO.java
+++ src/main/java/net/axel/wrm/domain/dtos/visit/VisitRequestDTO.java
@@ -1,10 +1,8 @@
 package net.axel.wrm.domain.dtos.visit;
 
 import jakarta.validation.constraints.NotNull;
-import net.axel.wrm.domain.enums.Status;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 public record VisitRequestDTO(
         Integer priority,
